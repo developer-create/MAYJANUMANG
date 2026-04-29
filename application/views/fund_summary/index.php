4fe0ec543f6d771e73668424a3ab7f0a2a025ddd
Developer <developer@example.com>
2026-04-29 17:01:50 +0530

    Revert "1. Fund Summary Controller Improvements:"
    This reverts commit 260a8b3423c75864582109553d8745f345895ebe.

--- application/views/fund_summary/index.php
+++ application/views/fund_summary/index.php
@@ -120,18 +120,6 @@
                                 </div>
                                 <div class="col-md-2">
                                     <div class="form-group">
-                                        <label>Approx Cost</label>
-                                        <select name="approx_cost_range" class="form-control">
-                                            <option value="">All Cost</option>
-                                            <option value="0-1" <?php echo ($filters['approx_cost_range'] == '0-1') ? 'selected' : ''; ?>>0-1 lakh</option>
-                                            <option value="1-5" <?php echo ($filters['approx_cost_range'] == '1-5') ? 'selected' : ''; ?>>1-5 lakh</option>
-                                            <option value="5-10" <?php echo ($filters['approx_cost_range'] == '5-10') ? 'selected' : ''; ?>>5-10 lakh</option>
-                                            <option value="10-above" <?php echo ($filters['approx_cost_range'] == '10-above') ? 'selected' : ''; ?>>10 Above lakh</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-2">
-                                    <div class="form-group">
                                         <label>Status</label>
                                         <select name="work_status" class="form-control">
                                             <option value="">All Status</option>
@@ -192,7 +180,6 @@
                                     <th>Status</th>
                                     <th>Approved Fund</th>
                                     <th>Approximate Cost</th>
-                                    <th>Approx Cost in Words</th>
                                     <th>Work Agency</th>
                                     <th>Remark</th>
                                     <th>Date</th>
@@ -376,7 +363,6 @@ jQuery(document).ready(function(){
                 d.filter_to_date = jQuery('#fundSummaryFilterForm input[name="to_date"]').val();
                 d.filter_work_status = jQuery('#fundSummaryFilterForm select[name="work_status"]').val();
                 d.filter_registration_no = jQuery('#fundSummaryFilterForm input[name="registration_no"]').val();
-                d.filter_approx_cost = jQuery('#fundSummaryFilterForm select[name="approx_cost_range"]').val();
             },
             dataSrc: function (json) {
                 if (json && typeof json.sumFiltered !== 'undefined') {
@@ -414,7 +400,7 @@ jQuery(document).ready(function(){
         pageLength: 20,
         responsive: true,
         scrollX: true,
-        order: [[22, 'desc']],
+        order: [[21, 'desc']],
         columnDefs: [
             { targets: [5, 16], orderable: false }
         ],
